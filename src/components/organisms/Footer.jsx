3fd7bfad692253f16562f3385a613dfdbb04b6c1
karinaesquivele <karinanayeliesquivele@gmail.com>
2024-10-11 14:51:21 -0600

    Add social links and final corrections

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -10,18 +10,37 @@ function Footer() {
       </div>
       <div className="w-full py-6 flex flex-col md:flex-row">
         <div className="footer__logo-col w-full md:w-1/4 md:p-0 pb-5">
-          <img src={logo} alt="Logo de la empresa" className="m-auto md:mr-auto md:ml-0" />
+          <img
+            src={logo}
+            alt="Logo de la empresa"
+            className="m-auto md:mr-auto md:ml-0"
+          />
         </div>
         <div className="footer__socials-col w-full md:w-1/4 pb-6 md:pb-0">
           <div className="socials">
-            <a href="#about" className="inline-block mr-4" target="_blank">
+            <a
+              href="https://www.linkedin.com/in/karina-nayeli-esquivel-espinoza-761756196/"
+              rel="noreferrer"
+              className="inline-block mr-4"
+              target="_blank"
+            >
               <img src={socialLinkedin} alt="Ícono redondo LinkedIn" />
             </a>
 
-            <a href="projects#" className="inline-block mr-4" target="_blank">
+            <a
+              href="https://github.com/karinaesquivele"
+              rel="noreferrer"
+              className="inline-block mr-4"
+              target="_blank"
+            >
               <img src={socialGithub} alt="Ícono redondo Github" />
             </a>
-            <a href="#contact" className="inline-block" target="_blank">
+            <a
+              href="https://www.behance.net/karinaesquivele"
+              rel="noreferrer"
+              className="inline-block"
+              target="_blank"
+            >
               <img src={socialBehance} alt="Ícono redondo Behance" />
             </a>
           </div>
@@ -29,16 +48,16 @@ function Footer() {
         <div className="footer__menu-col w-full md:w-1/2">
           <ul className="footer-menu w-full grid-cols-2 grid-rows-2 inline-grid lg:grid-cols-4 lg:grid-rows-1 items-center">
             <li className="py-3 lg:p-0 md:p-0">
-              <a href="#about" >Sobre mí</a>
+              <a href="#about">Sobre mí</a>
             </li>
             <li className="py-3 lg:p-0 md:p-0">
-              <a href="#projects" >Proyectos</a>
+              <a href="#projects">Proyectos</a>
             </li>
             <li className="py-3 lg:p-0 md:p-0">
-              <a href="#technologies" >Tecnologías</a>
+              <a href="#technologies">Tecnologías</a>
             </li>
             <li className="py-3 lg:p-0 md:p-0">
-              <a href="#contact" >Contacto</a>
+              <a href="#contact">Contacto</a>
             </li>
           </ul>
         </div>
